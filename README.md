cca7e5c060fda70da72ad37610cc8b9f76ff2ab1
haleemah-163 <abolaimon777@gmail.com>
2021-01-04 17:14:37 +0200

    my first project

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # myproject
 about me guessing game
+
+## LAB2
+
+#### I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
+##### Author: Haleemah Abu laimon
+
+
+##### Links and Resources
+[setting up an new code 201  roject ](https://codefellows.github.io/code-201-guide/curriculum/class-02/project_setup)
+
+##### Reflections and Comments
+##### first journal, submission questions, tools and resources used and learned
+*it was really good ,To be honest I didn't expect it would take me more than two hours,but even it had taken 4 hours of thinking, trying , reading and getting help from the nice TAs; it was fun and i feel happy when i see my work* 
+*I have no more questions until now and everything is good*
---
+++ js/app.js
@@ -0,0 +1,44 @@
+'use strict'
+// 
+//  '23';
+//  'jordanian';
+//  = 'yellow';
+// var  = 'engineering';
+var myplace = 'yes';
+var pplace = prompt('do you think I live in Amman?');
+if ( pplace.toLowerCase() === myplace ){
+    alert('hmmm try the next one then');
+} else {
+    alert('Braaaaavooo , yes I live in Ajloun');
+}
+var myage = 'yes';
+var page = prompt('do I seem more than 18?');
+if (page.toLowerCase() === myage ){
+    alert('absoluteley correct');
+}else {alert('hmm then how do you think could I have the schoolarship of coding???');}
+
+var mynationality = 'yes';
+var pnationality = prompt('do you think I am Jordanian?');
+if (pnationality.toLowerCase() === mynationality ){
+    alert('correct !!');
+}else {alert('wow , it seems you still do not know me, I am Jordanian, dude ');}
+var myfavcolor = 'yes';
+var pcolor = prompt('do you think I prefer Yellow color');
+if (pcolor.toLowerCase() === myfavcolor ) {alert('true , it is the color of the season');
+    
+} else {alert('to be honest, I love all colors but Yellow color is the best');}
+
+var myfeild = 'yes';
+var pfeild = prompt('am I doctor');
+if (pfeild.toLowerCase() === myfeild) {alert('well, that was my dad wanted me to be , but not me');
+    
+} else {alert('that is true ... actually i had never seen a doctor who is learning programming')
+    
+}
+
+// console.log('I live in Ajloun and I am 23, I am Jordanian my favourite color is Yellow and I finished my degree in engineering');
+alert('I live in Ajloun and I am 23, I am Jordanian my favourite color is Yellow and I finished my degree in engineering');
+alert('now it is your turn!');
+var username = prompt('what about you name, dear ?');
+document.write(' dear '+ username + ' you are more than welcome to my web site');
+alert(username + ' have a nice day');
